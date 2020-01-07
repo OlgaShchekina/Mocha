7c0033c2d2921c2f24ab248c6c4c08f94c4250cb
olgashchekina <olya6avg@gmail.com>
2020-01-07 13:29:18 -0800

    started project

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,7 @@ const {sum,mult} = require("../index.js");
 
 describe("function sum", () => {
     it("should function sum equals 6", () => {
-        assert.equals(sum(2,4),6);
+        assert.equal(sum(2,4),6);
     });
     it("should function sum not equals 0" , () => {
         assert.notEqual(sum(2,4),0);
